dde4379c1cbf9d4a420848efabd8c58e59a4bff5
qbuat <quentin.buat@cern.ch>
2015-02-18 16:39:58 -0800

    small fix

--- gen/__init__.py
+++ gen/__init__.py
@@ -8,7 +8,7 @@ SEEDS = [123456000 + i for i in range(0, 50)]
 MASSES = range(60, 205, 5)
 DSIDS = [300000 + i for i in xrange(len(MASSES))]
 
-PROD_DIR = '/d3pd'
+PROD_DIR = './d3pd'
 NTUPLE_DIR = './ntuple'
 FLAT_DRIVER = 'flatskim.py'
 FLAT_DRIVER_2 = 'flatskim2.py'
